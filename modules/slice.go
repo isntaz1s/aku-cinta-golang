1e93de9b3f1fe33990327317efd0b7f658d58944
isntaz1s <azis190407@gmail.com>
2024-11-24 16:38:14 +0700

    add make slice

--- modules/slice.go
+++ modules/slice.go
@@ -5,12 +5,14 @@ import "fmt"
 func Slice() {
 	person := [...]string{"Name", "Address", "City"}
 	month := [...]string{"Jan", "Feb", "Mar", "Apr", "May"}
+	days := make([]string, 7)
 
 	newPerson := person[2:]
 
 	newPerson2 := append(newPerson, "Country")
 
 	newMonth := month[0:2]
+	newDays := append(days, "Sunday", "Monday")
 
 	insertMonth := append(newMonth, "Jun")
 
@@ -19,4 +21,5 @@ func Slice() {
 	fmt.Println(newPerson2)
 	fmt.Println(len(newPerson2))
 	fmt.Println(insertMonth)
+	fmt.Println(newDays)
 }
